592b25cc89d3dbd52c07ced1c7298a2d36abfe02
abdimalik-omar <abdimalik.omar1@student.moringaschool.com>
2024-10-16 15:20:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    // Grab the input element by its ID
+    const input = document.getElementById('button');
+    
+    // Define the event handler function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener to the input element
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the addingEventListener function to activate the event listener
+  addingEventListener();
+  
